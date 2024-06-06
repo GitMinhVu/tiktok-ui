ac9a95f6a231eb480769507678167aa40d032827
GitMinhVu <tominhvuhn@gmail.com>
2024-06-06 14:39:58 +0700

    Xây dựng UI phần sidebar #1

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import config from '~/config';
+import style from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    LiveIcon,
+    UserGroupActiveIcon,
+    LiveActiveIcon,
+    HomeActiveIcon,
+} from '~/components/Icons';
+const cx = classNames.bind(style);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following Your"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
